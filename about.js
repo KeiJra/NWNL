36516da23b298f1b18787b461ff10af01561186c
KeiJra <86140196+KeiJra@users.noreply.github.com>
2025-08-10 16:53:23 +0700

    Add files via upload

---
+++ about.js
@@ -0,0 +1,6 @@
+document.getElementById("instagramBtn").addEventListener("click", () => {
+  window.open("https://www.instagram.com/keyzra___/", "_blank");
+});
+document.getElementById("twitterBtn").addEventListener("click", () => {
+  window.open("https://x.com/KeiiiOnX", "_blank");
+});
